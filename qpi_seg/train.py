548ab3c213c982bde64412dab79031834104835b
anoshqa <agupt130@jh.edu>
2026-06-14 19:03:46 +0000

    added validation

--- qpi_seg/train.py
+++ qpi_seg/train.py
@@ -12,7 +12,11 @@ import qpi_seg.file_charactersmatch as filetest
 import torch.nn.functional as F
 import qpi_seg.train_model as train_model
 import qpi_seg.validate as validate
+import qpi_seg.dicecoefficient as DC
+import subprocess
 import qpi_seg.mean_fn as mean
+from torch.utils.tensorboard import SummaryWriter
+
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 assert torch.cuda.is_available()
 #call Dataset
@@ -50,15 +54,39 @@ validationQPIdataset=qpi_seg.dataset.MIPDataset(image_folder,mask_folder,val_ima
 train_loader=DataLoader(trainQPIdataset, batch_size=1, shuffle=True)
 val_loader=DataLoader(validationQPIdataset, batch_size=1,shuffle=True)
 batch_image,batch_mask=next(iter(train_loader))
-print(batch_image.shape)
-print(batch_mask.shape)
+#print(batch_image.shape)
+#print(batch_mask.shape)
 #visualize.visualize(batch_image.squeeze(),batch_mask.squeeze())
 
 
 myUnet = UNet(depth=4,in_channels=1,out_channels=5, num_fmaps=4).to(device)
 loss=nn.CrossEntropyLoss()
 optimizer=torch.optim.Adam(myUnet.parameters())
+logger = SummaryWriter("runs/Unet")
 
 for epoch in range(3):
-    train_model.train_model(myUnet, train_loader, optimizer, loss, epoch, device=device)
-#get a validation metric
+    train_model.train_model(myUnet, train_loader, optimizer, loss, epoch, device=device,tb_logger=logger)
+    step= epoch * len(train_loader)
+    validate(myUnet,val_loader,loss_function=torch.nn.MSELoss(),metric=DC.DiceCoefficient(),step=step,device=device,tb_logger=logger)
+
+
+
+
+# Function to find an available port and launch TensorBoard on the browser
+def launch_tensorboard(log_dir):
+    import socket
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind(("", 0))
+        port = s.getsockname()[1]
+
+    tensorboard_cmd = f"tensorboard --logdir={log_dir} --port={port}"
+    process = subprocess.Popen(tensorboard_cmd, shell=True)
+    print(
+        f"TensorBoard started at http://localhost:{port}. \n"
+        "If you are using VSCode remote session, forward the port using the PORTS tab next to TERMINAL."
+    )
+    return process
+
+
+launch_tensorboard("runs")
