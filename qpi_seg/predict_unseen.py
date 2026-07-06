e16defe153c31112cc5cf840e3cb1de8d7f2fb0d
anoshqa <anoushkargupta8@gmail.com>
2026-07-06 14:15:51 -0400

    update predict_unseen

--- qpi_seg/predict_unseen.py
+++ qpi_seg/predict_unseen.py
@@ -16,34 +16,34 @@ device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cp
 model=UNet(depth=6,in_channels=1,out_channels=5, num_fmaps=32,final_activation=nn.Softmax()).to(device)
 
 #change the model path
-model_path=r'C:\Users\anous\OneDrive - Johns Hopkins\2026_DL_Janelia_course\UNet_model_1\checkpoint_epoch_190.pt'
+model_path=r'D:\TRAINING_DATA_FINAL\Model_fullsize_checkpoints\checkpoint_epoch_99.pt'
 #add map_location if using on CPU
-model_save=torch.load(model_path,map_location=torch.device('cpu'))
+#model_save=torch.load(model_path,map_location=torch.device('cpu'))
 
 #if using GPU uncomment the following two lines
-#assert torch.cuda.is_available()
-#model_save=torch.load(model_path)
+assert torch.cuda.is_available()
+model_save=torch.load(model_path)
 
 model.load_state_dict(model_save['model_state_dict'])
 model=model.to(device)
 
 #put your unseen images here
-test_images_folder=r'C:\Users\anous\Downloads\Test_anoushka'
+test_images_folder=r'D:\TRAINING_DATA_FINAL\TEST_MIP'
 
 #put folder where masks will be saved
-unet_masks_output_folder=r'C:\Users\anous\Downloads\Test_anoushka_masks'
+unet_masks_output_folder=r'D:\TRAINING_DATA_FINAL\TEST_MASK'
 
 
 test_files= os.listdir(test_images_folder)
 test_images = [tifffile.imread(os.path.join(test_images_folder,file)) for file in test_files]
-print(test_images[0].shape)
+#print(test_images[0].shape)
 
 out_file_name_stems=[os.path.splitext(file)[0][:30]+'_unet_masks.tiff'for file in test_files ]
 
 out_file_name_masks=[os.path.join(unet_masks_output_folder, file) for file in out_file_name_stems]
 
 from_np=transforms_v2.Lambda(lambda x: torch.from_numpy(x))
-transform = transforms_v2.CenterCrop((832,832))
+transform = transforms_v2.CenterCrop((1344,1344))
 
 #for victor's dataset may have to play with this
 norm_min=13300
@@ -53,6 +53,8 @@ clustermaps=[]
 
 
 for i in range(1):
+    #w=test_images[i].shape[0]
+    #h=test_images[i].shape[1]
     torch_test_image = from_np(test_images[i])
     torch_test_image=torch_test_image.float()
     img_norm= (torch_test_image - norm_min) / (norm_max - norm_min)
@@ -62,8 +64,8 @@ for i in range(1):
     
     img_norm2=transform(img_norm2)
     torch_prediction2=model(img_norm2) #prediction
-    clustermap=np.zeros((832,832))
-    torch_prediction=torch_prediction2.reshape(5,832,832).detach().cpu() #reshape to 5 channels and detach and cpu
+    clustermap=np.zeros((1344,1344))
+    torch_prediction=torch_prediction2.reshape(5,1344,1344).detach().cpu() #reshape to 5 channels and detach and cpu
     ch1_prediction = torch_prediction[0,:,:]
     ch2_prediction = torch_prediction[1,:,:]
     ch3_prediction= torch_prediction[2,:,:]
@@ -74,11 +76,9 @@ for i in range(1):
     clustermap[ch3_prediction>=0.5]=2
     clustermap[ch4_prediction>=0.5]=3
     clustermap[ch5_prediction>=0.5]=4
-    visualize.visualize(img_norm.squeeze(dim=0).cpu(), clustermap)
-    
-
-    #pad clustermap with 4 pixels to have (836,836) original size if input size is x you need to pad with (x - 832)/2
-    #padded_clustermap=np.pad(clustermap,((2,2),(2,2)),'constant',constant_values=(0,0))
+    transformed_mip=img_norm2.squeeze(dim=0)
+    transformed_mip=transformed_mip.squeeze(dim=0).cpu()
+    visualize.visualize(transformed_mip, clustermap)
     clustermaps.append(clustermap)
 
 #for i in range(len(out_file_name_masks)):
@@ -89,8 +89,6 @@ for i in range(1):
 
 #plot grid is a function that outputs 10 images (5 x 2 pattern)
 #pg.plot_grids(test_images[0:5],clustermaps[0:5] )
-
-visualize.visualize(img_norm.squeeze(dim=0).cpu(), clustermap)
 
 
 
