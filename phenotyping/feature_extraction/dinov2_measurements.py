9e2a24263636245ea1a26c375abb1f59cff16745
anoshqa <anoushkargupta8@gmail.com>
2026-07-12 19:58:21 -0400

    add masks folder

--- phenotyping/feature_extraction/dinov2_measurements.py
+++ phenotyping/feature_extraction/dinov2_measurements.py
@@ -5,7 +5,6 @@ import pandas as pd
 import requests
 import skimage
 import torch
-import matplotlib.pyplot as plt
 from PIL import Image
 from skimage.transform import resize
 from torchvision import transforms
@@ -20,34 +19,13 @@ transform1 = transforms.Compose([
 ])
 
 
-def save_gray_as_rgb_png(image):
-    """Save a 2D image as an RGB PNG using a matplotlib colormap without altering pixel values."""
-    img = np.asarray(image)
-    plt.imsave(img, cmap=cmap, vmin=0, vmax=4)
-
-
-def prepare_image_for_dinov2(image):
-    image_array = np.asarray(image)
-    if image_array.ndim == 2:
-        image_array = np.repeat(image_array[..., None], 3, axis=2)
-    image_array = image_array.astype(np.float32)
-    if image_array.max() > 1.0:
-        image_array = image_array / 255.0
-    return Image.fromarray(image_array)
 
 
 #imports images
-mask_folder=r'C:\Users\anous\OneDrive - Johns Hopkins\2026_datanalysis\dlmi2\phenotyping phase\Mask_proofread'
+mask_folder=r'F:\TRAINING_DATA_FINAL\Mask_resized_dino'
 
 masks=[skimage.io.imread(os.path.join(mask_folder, maskfile)) for maskfile in sorted(os.listdir(mask_folder))]
-mask_resized = [resize(image, (420, 420), order=0, anti_aliasing=False, preserve_range=True) for image in masks]
-
-png_paths = []
-for idx, image in enumerate(mask_resized):
-    png_path = os.path.join(os.getcwd(), f'tmp_mask_{idx}.png')
-    save_gray_as_rgb_png(image, image, png_path)
-    png_paths.append(png_path)
-    
+print(masks[0].shape)
     
 patch_size = dinov2_vits14.patch_size # patchsize=14
 
@@ -64,13 +42,13 @@ batch_size = 4
 total_features = []
 
 with torch.no_grad():
-    for i in range(0, len(png_paths), batch_size):
-        batch_images = png_paths[i:i+batch_size]
+    for i in range(0, len(masks), batch_size):
+        batch_masks = masks[i:i+batch_size]
 
         # Stack batch
         batch_tensors = torch.stack([
-            transform1(prepare_image_for_dinov2(Image.open(img_path).convert('RGB'))).to(dtype=torch.float32)
-            for img_path in batch_images
+            transform1(mask).to(dtype=torch.float32)
+            for mask in batch_masks
         ])
         batch_tensors = batch_tensors.to(device=device, dtype=torch.float32)
 
